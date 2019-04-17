dc2a86bc1eca9cdf04831897692c54ce6dd975ef
esisneros94 <edbpro999@hotmail.com>
2019-04-16 23:54:05 -0600

    Get this stuff

--- MattMobileDetail/EmployeeContent.Master.cs
+++ MattMobileDetail/EmployeeContent.Master.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using System.Web.Security;
 
 namespace MattMobileDetail
 {
@@ -13,18 +12,6 @@ namespace MattMobileDetail
         protected void Page_Load(object sender, EventArgs e)
         {
 
-        }
-
-        protected void btLogout_Click(object sender, EventArgs e)
-        {
-            Session.Clear();
-            Session.Abandon();
-            HttpContext.Current.Session.Abandon();
-            Response.Redirect("~/Non-Members/Login");
-
-            Response.Cache.SetCacheability(HttpCacheability.NoCache);
-            Response.Cache.SetExpires(DateTime.UtcNow.AddHours(-1));
-            Response.Cache.SetNoStore();
         }
     }
 }
